3791d822c7e214cb463f1f5c760cc986bb8c34ec
Andres Molina <amolina@andresmolina.org>
2015-09-24 01:16:27 -0300

    iteramos con las comunas
    Se agregan cambios para iterar con las comunas desde el sitio de redbanc

--- Spiders/redbanc_cl.py
+++ Spiders/redbanc_cl.py
@@ -1,63 +1,18 @@
-import xml.sax
+import xmltodict, json, urllib2
 
-class MovieHandler( xml.sax.ContentHandler ):
-   def __init__(self):
-      self.CurrentData = ""
-      self.type = ""
-      self.format = ""
-      self.year = ""
-      self.rating = ""
-      self.stars = ""
-      self.description = ""
+def dataxml(uri):
+    file = urllib2.urlopen(uri)
+    data = file.read()
+    file.close()
+    
+    return data
 
-   # Call when an element starts
-   def startElement(self, tag, attributes):
-      self.CurrentData = tag
-      if tag == "movie":
-         print "*****Movie*****"
-         title = attributes["title"]
-         print "Title:", title
+ #Descargamos el listado de Comunas
+comunas = xmltodict.parse(dataxml("http://www.redbanc.cl/portal_redbanc/browse?pagina=portal_redbanc/comunas.xml"))
 
-   # Call when an elements ends
-   def endElement(self, tag):
-      if self.CurrentData == "type":
-         print "Type:", self.type
-      elif self.CurrentData == "format":
-         print "Format:", self.format
-      elif self.CurrentData == "year":
-         print "Year:", self.year
-      elif self.CurrentData == "rating":
-         print "Rating:", self.rating
-      elif self.CurrentData == "stars":
-         print "Stars:", self.stars
-      elif self.CurrentData == "description":
-         print "Description:", self.description
-      self.CurrentData = ""
+#Iteramos con la region metropolitana
+comunasrm=comunas["retorno"]["comuna-metropolitana"]["comuna"]
+#Al iterar meteremos las comunas en una base de datos local
+for comuna in comunasrm:
+    print comuna
 
-   # Call when a character is read
-   def characters(self, content):
-      if self.CurrentData == "type":
-         self.type = content
-      elif self.CurrentData == "format":
-         self.format = content
-      elif self.CurrentData == "year":
-         self.year = content
-      elif self.CurrentData == "rating":
-         self.rating = content
-      elif self.CurrentData == "stars":
-         self.stars = content
-      elif self.CurrentData == "description":
-         self.description = content
-  
-if ( __name__ == "__main__"):
-   
-   # create an XMLReader
-   parser = xml.sax.make_parser()
-   # turn off namepsaces
-   parser.setFeature(xml.sax.handler.feature_namespaces, 0)
-
-   # override the default ContextHandler
-   Handler = MovieHandler()
-   parser.setContentHandler( Handler )
-   
-   parser.parse("prueba.xml")
