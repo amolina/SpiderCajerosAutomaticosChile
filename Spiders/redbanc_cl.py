d0e8d3f8c08fbd5e0e32e596fd13ae41b064b0ad
Andres Molina <amolina@andresmolina.org>
2015-09-24 23:08:51 -0300

    Agregamos Codigo para iterar
    Se agrega codigo para iterar por comunas RM y comunas regiones

--- Spiders/redbanc_cl.py
+++ Spiders/redbanc_cl.py
@@ -7,12 +7,44 @@ def dataxml(uri):
     
     return data
 
+#declaramos variables a utilizar mas tarde
+
+comunascl={}
+comunasrms={}
+comunasregioness={}
+
  #Descargamos el listado de Comunas
 comunas = xmltodict.parse(dataxml("http://www.redbanc.cl/portal_redbanc/browse?pagina=portal_redbanc/comunas.xml"))
 
 #Iteramos con la region metropolitana
 comunasrm=comunas["retorno"]["comuna-metropolitana"]["comuna"]
+
+
 #Al iterar meteremos las comunas en una base de datos local
+cuenta=0
 for comuna in comunasrm:
-    print comuna
+    cuenta=cuenta+1
+    #lacomuna={}
+    codigo=comuna["id"]
+    nombre=comuna["nombre"].encode("utf-8")
+    #lacomuna[codigo]=nombre
+    #lacomuna['nombre']=nombre
+    comunasrms[codigo]=nombre
+
+#iteramos por regiones
+comunasreg=comunas["retorno"]["comuna-regional"]["comuna"]
+#Al iterar meteremos las comunas en una base de datos local
+cuenta=0
+for comuna in comunasreg:
+    cuenta=cuenta+1
+    #lacomuna={}
+    codigo=comuna["id"]
+    nombre=comuna["nombre"].encode("utf-8")
+    #lacomuna[codigo]=nombre
+    #lacomuna['nombre']=nombre
+    comunasregioness[codigo]=nombre
+
+print comunasrms
+print "-----"
+print comunasregioness
 
