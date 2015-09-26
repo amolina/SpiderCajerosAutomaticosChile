547c52e8bd12c97e7b6a1169fc214678b4a67eb5
Andres Molina <amolina@andresmolina.org>
2015-09-26 02:47:49 -0300

    Obtiene cajeros
    Se agrego la rutina para iterar por todas las regiones y poder obtener
    el listado de cajeros por cada comuna

--- Spiders/redbanc_cl.py
+++ Spiders/redbanc_cl.py
@@ -8,9 +8,16 @@ def dataxml(uri):
 
 def savejson(data,archivo):
     fn = os.path.join(os.path.dirname(__file__), 'data\/json\/'+archivo)
-    print fn
     with open(fn, 'w+') as fp:
         json.dump(data, fp)
+
+def loadjson(archivo):
+    fn = os.path.join(os.path.dirname(__file__), 'data\/json\/'+archivo)
+    with open(fn) as json_data:
+        d = json.load(json_data)
+        json_data.close()
+        return d
+    
 
 
 #declaramos variables a utilizar mas tarde
@@ -18,6 +25,7 @@ def savejson(data,archivo):
 comunascl={}
 comunasrms={}
 comunasregioness={}
+cajeroschile={}
 
  #Descargamos el listado de Comunas
 comunas = xmltodict.parse(dataxml("http://www.redbanc.cl/portal_redbanc/browse?pagina=portal_redbanc/comunas.xml"))
@@ -56,5 +64,70 @@ comunascl["rm"]=comunasrms
 savejson(comunascl,"comunas.json")
 
 #iteramos por las comunas para obtener data de cajeros
+
+ 
+comunas=loadjson("comunas.json")
+#Iterando en las comunas de la region metropolitana
+
+#cajeros = xmltodict.parse(dataxml("http://www.redbanc.cl/portal_redbanc/browse?pagina=portal_redbanc/Localizador_cajeros.xml&idioma=0&id_comuna=13110"))
+ 
+# la estructura del xml cambio el  2011, ahora en lugar de mostrar el codigo de cajero, muestra la cantidad de cajeros que hay en la direccion, es menos util que la primera version del XML, previo al 2011
+
+
+
+
+for id, comuna in comunas["rm"].items():
+    cajeros = xmltodict.parse(dataxml("http://www.redbanc.cl/portal_redbanc/browse?pagina=portal_redbanc/Localizador_cajeros.xml&idioma=0&id_comuna="+id))
+    cuenta=0
+    if cajeros["retorno"].has_key("cajero"):
+        for cajeross in cajeros["retorno"]["cajero"]:
+            cuenta=cuenta+1
+            cajerob={}
+            cajerob["comuna"]=id
+            cajerob["region"]="rm"
+            cajerob["cantidad"]=cajero["numero"]
+            cajerob["institucion"]=cajero["institucion"]
+            cajerob["direccion"]=cajero["direccion"]
+            cajerob["tipolocal"]=cajero["tipolocal"]
+            cajerob["cirrus"]=cajero["cirrus"]
+            cajerob["plus"]=cajero["plus"]
+            cajerob["tipocajero"]=cajero["tipocajero"]
+            cajerob["horario"]=cajero["horario"]
+            cajeroschile[cuenta]=cajerob
+    else:
+        print id
+        
+
+
+        
+for id, comuna in comunas["regiones"].items():
+    cajeros = xmltodict.parse(dataxml("http://www.redbanc.cl/portal_redbanc/browse?pagina=portal_redbanc/Localizador_cajeros.xml&idioma=0&id_comuna="+id))
+    cuenta=0
+    if cajeros["retorno"].has_key("cajero"):
+        for cajeross in cajeros["retorno"]["cajero"]:
+            cuenta=cuenta+1
+            cajerob={}
+            cajerob["comuna"]=id
+            cajerob["region"]="regiones"
+            cajerob["cantidad"]=cajero["numero"]
+            cajerob["institucion"]=cajero["institucion"]
+            cajerob["direccion"]=cajero["direccion"]
+            cajerob["tipolocal"]=cajero["tipolocal"]
+            cajerob["cirrus"]=cajero["cirrus"]
+            cajerob["plus"]=cajero["plus"]
+            cajerob["tipocajero"]=cajero["tipocajero"]
+            cajerob["horario"]=cajero["horario"]
+            cajeroschile[cuenta]=cajerob
+    else:
+        print id
+        
+
+
  
 
+
+print cajeroschile
+
+"""     for cajero in cajeros["retorno"]["cajero"]:
+        print cajero["direccion"]
+"""
